24b27664c07cf345ed37f26a48edfb8d1f701965
Ashutosh Yadav <tntzxtech@gmail.com>
2025-04-23 18:02:42 +1000

    Added heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24 gap-8">
+      <h1 className="text-8xl pb-20 ">Flash Dynamics Syndicate Development Team</h1>
+
+
+
+
       <Link href={"/ashutosh"}>
         <div className="border-2 border-gray-300 w-76 rounded-lg p-6 bg-white shadow-md">
           <h1 className="text-2xl font-bold mb-4">Visit Ashutosh Page</h1>
