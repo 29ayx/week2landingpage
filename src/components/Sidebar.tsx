079ee47a1c1d44ab1f39c7eba7b458913bfa7962
Ayush kumar Singh <ayusharon209@gmail.com>
2025-04-28 12:42:19 +0530

    added sidebar little bit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+    return (
+        <>
+        <div className="border-2 m-3 border-gray-300 flex flex-col rounded-xl p-4 w-60">
+            <button type="button" className="border-2 p-3">
+                Online Services
+            </button>
+
+        </div>
+        </>
+    )
+}
